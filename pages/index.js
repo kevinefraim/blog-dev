4f7b146f9a6058c9688d57e5274df22eaae58451
kevinefraim <kevinefraim02@gmail.com>
2022-05-17 14:46:44 -0300

    setting index page

--- pages/index.js
+++ pages/index.js
@@ -20,22 +20,20 @@ const Home = () => {
   };
   return (
     <AppLayout>
-      <section className="grid place-items-center place-content-center h-full w-full   ">
-        <div className="flex flex-col items-center bg-white shadow-2xl p-20  rounded">
-          <Image width={125} height={125} src="/logo.png" alt="devter" />
-          <h1 className="text-red-800 text-xl font-extrabold mb-0 mt-2">
-            BlogDev
-          </h1>
-          <h2 className="text-red-400 text-lg">Media Blog with NextJs</h2>
-          <div className="mt-2">
-            {user === USER_STATES.NOT_LOGGED && (
-              <BtnLog handleClick={handleClick} />
-            )}
-            {/* {user && user.avatar && <Avatar user={user} text={user.username} />} */}
-            {user === USER_STATES.NOT_KNOWN && <span>Loading ...</span>}
-          </div>
+      <div className="h-[100%] flex flex-col justify-center items-center">
+        <Image width={125} height={125} src="/logo.png" alt="devter" />
+        <h1 className="text-red-800 text-xl font-extrabold mb-0 mt-2">
+          BlogDev
+        </h1>
+        <h2 className="text-red-400 text-lg">Media Blog with NextJs</h2>
+        <div className="mt-2">
+          {user === USER_STATES.NOT_LOGGED && (
+            <BtnLog handleClick={handleClick} />
+          )}
+          {/* {user && user.avatar && <Avatar user={user} text={user.username} />} */}
+          {user === USER_STATES.NOT_KNOWN && <span>Loading ...</span>}
         </div>
-      </section>
+      </div>
     </AppLayout>
   );
 };
